eb239a56d33ccf5a94d62b569ea7f806095dfd1c
alex-isidro <alexander.d.isidro@gmail.com>
2026-05-14 11:46:19 -0300

    projeto inicial

---
+++ src/main/java/fiap/com/br/petcarehub/PetCarehubApplication.java
@@ -0,0 +1,13 @@
+package fiap.com.br.petcarehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCarehubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetCarehubApplication.class, args);
+    }
+
+}
