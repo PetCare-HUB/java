e70e79c506dc7a3fa5bd501d61e1ed2e113245ef
alex-isidro <alexander.d.isidro@gmail.com>
2026-05-15 02:42:03 -0300

    refactor: add toDTO

--- src/main/java/fiap/com/br/petcarehub/controller/PetController.java
+++ src/main/java/fiap/com/br/petcarehub/controller/PetController.java
@@ -1,5 +1,8 @@
 package fiap.com.br.petcarehub.controller;
 
+import fiap.com.br.petcarehub.dto.ClinicaResumoDTO;
+import fiap.com.br.petcarehub.dto.PetResponseDTO;
+import fiap.com.br.petcarehub.dto.ResponsavelResumoDTO;
 import fiap.com.br.petcarehub.entity.Pet;
 import fiap.com.br.petcarehub.service.PetService;
 import org.springframework.http.HttpStatus;
@@ -18,32 +21,56 @@ public class PetController {
     }
 
     @GetMapping
-    public List<Pet> findAll() {
-        return service.findAll();
+    public List<PetResponseDTO> findAll() {
+        return service.findAll()
+                .stream()
+                .map(this::toDTO)
+                .toList();
     }
 
     @GetMapping("/{id}")
-    public Pet findById(@PathVariable Long id) {
-        return service.findById(id);
+    public PetResponseDTO findById(@PathVariable Long id) {
+        return toDTO(service.findById(id));
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Pet add(@RequestBody Pet pet) {
-        return service.add(pet);
+    public PetResponseDTO add(@RequestBody Pet pet) {
+        return toDTO(service.add(pet));
     }
 
     @PutMapping("/{id}")
-    public Pet update(
+    public PetResponseDTO update(
             @PathVariable Long id,
             @RequestBody Pet pet
     ) {
-        return service.update(id, pet);
+        return toDTO(service.update(id, pet));
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         service.delete(id);
+    }
+
+    private PetResponseDTO toDTO(Pet pet) {
+
+        return new PetResponseDTO(
+
+                pet.getId(),
+                pet.getNome(),
+                pet.getEspecie().name(),
+                pet.getPesoKg(),
+
+                new ResponsavelResumoDTO(
+                        pet.getResponsavel().getId(),
+                        pet.getResponsavel().getNome()
+                ),
+
+                new ClinicaResumoDTO(
+                        pet.getClinica().getId(),
+                        pet.getClinica().getNome()
+                )
+        );
     }
 }
